21311f64c1108c06944d2a6f65a6a7060cae9b3d
Agustin Vidal <tin_tilin@hotmail.com>
2018-09-06 10:42:05 -0700

    Primeros pasos JAVA

---
+++ Declaracion_Operadores.java
@@ -0,0 +1,33 @@
+public class Declaracion_Operadores {
+public static void  main(String[] args){
+    /* int a=5;
+     int b;
+     b=7;
+
+     int c=b+a;
+         //c=b-a;
+         //c=b*a;
+   /*0 double q=5; este operador
+    double w;      solo se pueda
+    w=10;          usar con un
+    double =w/q;   double
+
+     //c++;//incrementa en uno
+     //c+=6;//incrementa en 6
+     //c-=6;//decrementa en 6
+
+    System.out.println(c);*/
+
+    /*final double apulgadas=2.54; //declarar una constante y no se [uede modificar
+    double cm=6;
+    double resultado=cm/apulgadas;
+
+    System.out.println("En "+ cm + " cm hay " + resultado + " pulgadas");//+ se ua para concatenar
+*/
+    int operador1,operador2, resultado;// se puede declarar a si y despues ir uilizando los datos
+    operador1 =8;//iniciando un dato
+    operador2=7;
+    resultado=operador1+operador2;
+    System.out.println(resultado);
+}
+}
---
+++ PrimerosPasosSRC.java
@@ -0,0 +1,18 @@
+public class PrimerosPasosSRC
+{
+    public static void  main(String[] args)
+    {
+        //byte edad=35;//declaracion de iniciacion de una variable en unicalinea
+        byte edad;
+        edad=35;
+
+   /* la siguiente instruccion imprime en la consola
+   (System.out) el valor de la variable
+    */
+    System.out.println(edad);
+
+    edad=75;
+
+    System.out.println(edad);
+    }
+}
